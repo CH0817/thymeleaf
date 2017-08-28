e9934430674191232f9a12fe8dc7ae75780f4640
Rex Yu <yu.chenhang@gmail.com>
2017-08-28 17:46:22 +0800

    Delete empty scan interface

--- src/main/java/tw/com/rex/controller/ScanController.java
+++
@@ -1,6 +0,0 @@
-package tw.com.rex.controller;
-
-/**
- * empty controller interface for Spring component scan
- */
-public interface ScanController {}
--- src/main/java/tw/com/rex/service/ScanService.java
+++
@@ -1,6 +0,0 @@
-package tw.com.rex.service;
-
-/**
- * empty service interface for Spring component scan
- */
-public interface ScanService {}
